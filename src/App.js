15efb69a850599ff13dab2d499a22814ac6a7667
Rayyan <ralam3@ocdsb.ca>
2021-10-28 10:54:57 -0400

    Pushing up final clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* Header */}
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          {/* Always have default "/" on the bottom */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+      {/* Banner */}
+      {/* Cards  */}
+      {/* Footer */}
+      {/* Search */}
+      {/* ... */}
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+function Fotter() {
+  return (
+    <div className="footer">
+      <p>© 2020 This is a Demo Airbnb clone! No rights reserved</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
+
+export default Fotter;
